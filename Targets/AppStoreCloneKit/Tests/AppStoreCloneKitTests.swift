178de6e463c4c0458273cc1f66aef7d215c3517c
Junghun Seo <jhseo.dev@gmail.com>
2022-05-18 08:14:25 +0900

    feat: Tuist 초기 설정

---
+++ Targets/AppStoreCloneKit/Tests/AppStoreCloneKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class AppStoreCloneKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("AppStoreCloneKit", "AppStoreCloneKit")
+    }
+}
---
+++ Targets/AppStoreCloneUI/Tests/AppStoreCloneUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class AppStoreCloneUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("AppStoreCloneUI", "AppStoreCloneUI")
+    }
+}
